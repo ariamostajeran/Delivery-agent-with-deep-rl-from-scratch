4985c7586a39991f60080130ccb9e067587ecb90
koengalle <k.galle@student.tue.nl>
2024-05-07 22:33:08 +0200

    Cleanup code + Add comments

--- agents/value_agent.py
+++ agents/value_agent.py
@@ -14,28 +14,24 @@ from agents import BaseAgent
 
 
 class ValueAgent(BaseAgent):
-    def __init__(self, nr_states, stateSpace, actionSpace, gamma, cols, P, R):
-        self.states = nr_states
+    def __init__(self, stateSpace, actionSpace, gamma, cols, P, R):
         self.stateSpace = stateSpace
-        self.actions = actionSpace
+        self.actionSpace = actionSpace
         self.gamma = gamma
-        self.V = [0.0 for _ in range(len(self.states))]
-        self.pi = [0 for _ in range(len(self.states))]
+        self.V = [0.0 for _ in range(len(self.stateSpace))]
+        self.pi = [0 for _ in range(len(self.stateSpace))]
         self.cols = cols
-        self.rows = int(len(self.states) / cols)
         self.P = P
         self.R = R
 
     def pointer(self, state: tuple[int, int]):
-        # return state[0] + state[1] * self.cols
-        # print(state, state[0] + state[1] * self.cols)
         return state[0] + state[1] * self.cols
 
     def update(self, stateSpace):
         # Policy evaluation
         for state in stateSpace:
             best_value = -math.inf
-            for action in range(4):
+            for action in self.actionSpace:
                 expected_value = self.R[action][self.pointer(state)] + self.gamma*sum(self.P[action][self.pointer(state)][self.pointer(next_state)] * self.V[self.pointer(next_state)] for next_state in self.stateSpace)
                 if expected_value > best_value:
                     best_value = copy.copy(expected_value)
@@ -44,43 +40,13 @@ class ValueAgent(BaseAgent):
         # Policy control
         for state in stateSpace:
             best_value, best_action = -math.inf, None
-            for action in range(4):
+            for action in self.actionSpace:
                 expected_value = self.R[action][self.pointer(state)] + self.gamma*sum(self.P[action][self.pointer(state)][self.pointer(next_state)] * self.V[self.pointer(next_state)] for next_state in self.stateSpace)
                 if expected_value > best_value:
                     best_value = copy.copy(expected_value)
                     best_action = copy.copy(action)
             self.pi[self.pointer(state)] = best_action
 
-        # Debugging
-        # print(self.V)
-        # print(self.pi)
-
-        # expected_value = 0
-
-        # next_states = self.states
-        # for next_state in next_states:
-        #     prob = self.P[action][self.pointer(state)][next_state]
-        #     expected_value += prob * self.V[next_state]
-
-        # self.V[self.pointer(state)] = reward + self.gamma * expected_value
-
     def take_action(self, state: tuple[int, int]) -> int:
-        print(self.pi, self.pi[self.pointer(state)], state, self.pointer(state))
+        # Look up the best action to take for the given state in the policy
         return self.pi[self.pointer(state)]
-
-        # max_value = float("-inf")
-        # best_action = None
-
-        # for action in self.actions:
-        #     expected_value = 0
-
-        #     next_states = self.states
-        #     for next_state in next_states:
-        #         prob = self.P[action][self.pointer(state)][next_state]
-        #         expected_value += prob * self.V[next_state]
-
-        #     if expected_value > max_value:
-        #         max_value = expected_value
-        #         best_action = action
-
-        # return best_action
--- train.py
+++ train.py
@@ -60,24 +60,6 @@ def parse_args():
 def pointer(i, rows):
     return (i % (rows), i // (rows))
 
-# def get_P_matrix(grid, size, actions):
-#     # Initialize probability matrix
-#     P = [[[0 for _ in range(size)] for _ in range(size)] for _ in range(len(actions))]
-#     for action in enumerate(P):
-#         for p in range(size):
-#             for q in range(size):
-#                 c_from = pointer(p, grid.shape[0])
-#                 c_to = pointer(q, grid.shape[0])
-#                 if action[0] == 0 and c_from[0] == c_to[0] and c_from[1] == c_to[1] - 1 and (grid[c_from[0]][c_from[1]] == 0 or grid[c_from[0]][c_from[1]] == 4 or grid[c_from[0]][c_from[1]] == 3) and (grid[c_to[0]][c_to[1]] == 0 or grid[c_to[0]][c_to[1]] == 4 or grid[c_to[0]][c_to[1]] == 3):
-#                     P[action[0]][p][q] = 1
-#                 elif action[0] == 1 and c_from[0] == c_to[0] and c_from[1] == c_to[1] + 1 and (grid[c_from[0]][c_from[1]] == 0 or grid[c_from[0]][c_from[1]] == 4 or grid[c_from[0]][c_from[1]] == 3) and (grid[c_to[0]][c_to[1]] == 0 or grid[c_to[0]][c_to[1]] == 4 or grid[c_to[0]][c_to[1]] == 3):
-#                     P[action[0]][p][q] = 1
-#                 elif action[0] == 2 and c_from[0] == c_to[0] + 1 and c_from[1] == c_to[1] and (grid[c_from[0]][c_from[1]] == 0 or grid[c_from[0]][c_from[1]] == 4 or grid[c_from[0]][c_from[1]] == 3) and (grid[c_to[0]][c_to[1]] == 0 or grid[c_to[0]][c_to[1]] == 4 or grid[c_to[0]][c_to[1]] == 3):
-#                     P[action[0]][p][q] = 1
-#                 elif action[0] == 3 and c_from[0] == c_to[0] - 1 and c_from[1] == c_to[1] and (grid[c_from[0]][c_from[1]] == 0 or grid[c_from[0]][c_from[1]] == 4 or grid[c_from[0]][c_from[1]] == 3) and (grid[c_to[0]][c_to[1]] == 0 or grid[c_to[0]][c_to[1]] == 4 or grid[c_to[0]][c_to[1]] == 3):
-#                     P[action[0]][p][q] = 1
-#     return P
-
 def moved_down(c_from, c_to):
     return c_from[0] == c_to[0] and c_from[1] == c_to[1] - 1
 
@@ -93,6 +75,7 @@ def moved_right(c_from, c_to):
 def hit_wall(grid, c_from, c_to):
     return grid[c_from[0]][c_from[1]] == 1 or grid[c_to[0]][c_to[1]] == 1 or grid[c_from[0]][c_from[1]] == 2 or grid[c_to[0]][c_to[1]] == 2
 
+# Construct the probability matrix
 def get_P_matrix(grid, size, actions):
     # Initialize probability matrix
     P = [[[0 for _ in range(size)] for _ in range(size)] for _ in range(len(actions))]
@@ -101,20 +84,27 @@ def get_P_matrix(grid, size, actions):
             for q in range(size):
                 c_from = pointer(p, grid.shape[0])
                 c_to = pointer(q, grid.shape[0])
+                # Have action to go down, next state is actually down and no obstacle is hit
                 if action[0] == 0 and moved_down(c_from, c_to) and (not hit_wall(grid, c_from, c_to)):
                     P[action[0]][p][q] = 1
+                # Have action to go up, next state is actually up and no obstacle is hit
                 elif action[0] == 1 and moved_up(c_from, c_to) and (not hit_wall(grid, c_from, c_to)):
                     P[action[0]][p][q] = 1
+                # Have action to go left, next state is actually left and no obstacle is hit
                 elif action[0] == 2 and moved_left(c_from, c_to) and (not hit_wall(grid, c_from, c_to)):
                     P[action[0]][p][q] = 1
+                # Have action to go right, next state is actually right and no obstacle is hit
                 elif action[0] == 3 and moved_right(c_from, c_to) and (not hit_wall(grid, c_from, c_to)):
                     P[action[0]][p][q] = 1
+
+    # Else we hit an obstacle, which means that our state does not change
     for action in range(4):
         for p in range(size):
             if sum(P[action][p]) == 0:
                 P[action][p][p] = 1
     return P
 
+# Get new state given a current state and action
 def move(state: tuple[int, int], action: int):
     if action == 0:
         return (state[0], state[1] + 1)
@@ -125,22 +115,29 @@ def move(state: tuple[int, int], action: int):
     elif action == 3:
         return (state[0] + 1, state[1])
 
+# Construct the reward matrix
 def get_R_matrix(grid, size, actions):
+    # Initialize reward matrix
     R = [[0 for _ in range(size)] for _ in range(len(actions))]
     for action in range(len(actions)):
         for p in range(size):
             c_from = pointer(p, grid.shape[0])
             c_to = move(pointer(p, grid.shape[0]), action)
+            # Start from an illegal cell (boundary or obstacle)
             if grid[c_from[0]][c_from[1]] == 1 or grid[c_from[0]][c_from[1]] == 2:
                 R[action][p] = -1000
+            # Start from a legal cell (empty) and move into an illegal cell (boundary or obstacle)
             elif grid[c_to[0]][c_to[1]] == 1 or grid[c_to[0]][c_to[1]] == 2:
                 R[action][p] = -2
+            # Start from a legal cell (empty) and move into a legal cell (empty)
             elif grid[c_to[0]][c_to[1]] == 0:
                 R[action][p] = -1
+            # Start from a legal cell (empty) and move into the target (dirt or charger)
             elif grid[c_to[0]][c_to[1]] == 3 or grid[c_to[0]][c_to[1]] == 4:
                 R[action][p] = 10
     return R
 
+# Construct a state for each cell in the grid
 def make_states(cols: int, rows: int):
     stateSpace = []
     for row in range(rows):
@@ -170,35 +167,22 @@ def main(
         stateSpace = make_states(env.grid.shape[0], env.grid.shape[1])
         actionSpace = range(4)
         P = get_P_matrix(env.grid, len(nr_states), actionSpace)
-        # for p in P[0]:
-        #     print(p)
-        # for p in P[1]:
-        #     print(p)
         R = get_R_matrix(env.grid, len(nr_states), actionSpace)
-        agent = ValueAgent(nr_states, stateSpace, actionSpace, 0.9, env.grid.shape[0], P, R)
+        agent = ValueAgent(stateSpace, actionSpace, 0.9, env.grid.shape[0], P, R)
 
         # Always reset the environment to initial state
         state = env.reset()
         for _ in trange(iters):
 
+            # Update expected value matrix and policy
             agent.update(stateSpace)
+            # Get best action to take based on updated policy
             action = agent.take_action(state)
+            # Perform the step in the environment
             state, _, terminated, _ = env.step(action)
+            # Perform another run when target is reached
             if terminated:
                 env.reset()
-
-            # Agent takes an action based on the latest observation and info.
-            # action = agent.take_action(state)
-
-            # # The action is performed in the environment
-            # state, reward, terminated, info = env.step(action)
-
-            # # If the final state is reached, stop.
-            # if terminated:
-            #     env.reset()
-            #     # break
-
-            # agent.update(state, reward, info["actual_action"])
 
         # Evaluate the agent
         Environment.evaluate_agent(grid, agent, iters, sigma, random_seed=random_seed)
